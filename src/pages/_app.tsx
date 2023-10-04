e5dc9f0742291819b0b561e32db41afe3bbdeb6f
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-10-04 00:03:03 -0300

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from "next/app";
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ["400", "700", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return(
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import styles from '../styles/global.module.scss'
+
+export default function Page() {
+  return <h1 className={styles.title}>Hello, Next.js!</h1>;
+}
